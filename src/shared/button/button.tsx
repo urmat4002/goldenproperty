df848c3cbb4925a1f10afa65e351d7ff293e73a0
DAYAN <binaryones.gQ@gmail.com>
2023-12-27 22:22:16 +0600

    build: button components

---
+++ src/shared/button/button.tsx
@@ -0,0 +1,24 @@
+import style from './button.module.scss';
+import { FC } from 'react';
+import { ButtonProps } from './types/button.types';
+
+export const Button: FC<ButtonProps> = ({
+  type,
+  types,
+  children,
+  content,
+  onClick,
+}) => {
+  const typesButton = style[`button--${types}`];
+
+  return (
+    <button
+      type={type}
+      className={`${style.button} ${typesButton}`}
+      onClick={onClick}
+    >
+      {content}
+      {children}
+    </button>
+  );
+};
