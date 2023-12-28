26952eff2a6b95d6d377ad16661ccfaaa1c9a5dc
DAYAN <binaryones.gQ@gmail.com>
2023-12-28 13:57:53 +0600

    refactor(sass) & build: add header section make havbar

--- src/widget/header/header.tsx
+++ src/widget/header/header.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { FC } from 'react';
+import style from './header.module.scss';
+import Navbar from '../../entities/navbar/navbar';
+import { Button } from '../../shared/button/button';
+import { WhatsApp } from '../../shared/icons/whats-app';
+import { GuldemLogo } from '../../shared/icons/guldem-logo';
 
-const Header = () => {
+
+const Header: FC = () => {
   return (
-    <div>Header</div>
+    <header className={style.header}>
+      <div className={style.header__container}>
+        <div className={style.header__logo}>
+          <GuldemLogo />
+        </div>
+        <Navbar />
+        <div className={style.header__options}>
+          <Button
+            types="primary"
+            content='Связатся с нами'>
+            <WhatsApp color='white' size={32}/>
+          </Button>
+        </div>
+      </div>
+    </header>
   )
 }
 
