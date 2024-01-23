e46308b78e3b6e5527f03ef56e73cf89b38945f4
Dayan Ulanov <dayanulanov@Dayans-MacBook-Pro.local>
2024-01-23 13:14:10 +0600

    refactor: add adaptive hero $ search

--- src/shared/ui/search/search.tsx
+++ src/shared/ui/search/search.tsx
@@ -4,7 +4,7 @@ import style from './search.module.scss';
 const Search = () => {
   return (
     <div className={style.search}>
-      <div className={style.search__container}>
+      <div className={style.search__contain}>
         <div className={style.search__form}>
           <SearchIcon color="#b9bcbe" className={style.search__icon} />
           <input
@@ -23,7 +23,6 @@ const Search = () => {
         </div>
         <button className={style.search__button}>
           Поиск
-          <SearchIcon />
         </button>
       </div>
     </div>
--- src/widget/hero/hero-home/hero.tsx
+++ src/widget/hero/hero-home/hero.tsx
@@ -1,18 +1,22 @@
 import Search from '../../../shared/ui/search/search';
 import { Typography } from '../../../shared/ui/typography/typography';
 import style from './hero.module.scss';
+import data from './data/hero.db.json';
 
 const Hero = () => {
   return (
     <div className={style.hero}>
+      <img src={data.img} alt={''} className={style.hero__img}/> 
       <div className={style.hero__container}>
         <div className={style.hero__title}>
-          <Typography variant="label" weight="regular">
+          <Typography variant="h1" weight="bold">
             Найди свой дом мечты вместе с нами!
           </Typography>
         </div>
-        <Search />
-      </div>
+        <div className={style.hero__search}>
+          <Search />
+        </div>
+      i</div>
     </div>
   );
 };
