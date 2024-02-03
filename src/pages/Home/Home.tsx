e74be059230746192780506f6833d2fbda5ec595
urmat4002 <urmat4002@gmail.com>
2024-02-03 12:49:55 +0600

    feat: create form

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Form } from '@/features';
 import { Button, Input } from '@/shared/ui';
 
 export const Home = () => {
@@ -5,6 +6,10 @@ export const Home = () => {
     <div>
       <Button type="primary">Button</Button>
       <Input placeholder="City" />
+      <Form
+        title={'Do you have any questions?'}
+        subTitle={'Leave your contacts and we will get in touch'}
+      />
     </div>
   );
 };
