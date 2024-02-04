9958ca38875644ced350b9644a3af4f6724cf040
Maxim <334890@gmail.com>
2024-02-04 19:29:27 +0600

    implement floating whatsapp button

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import '@app/styles/index.scss';
-import { Provider } from 'react-redux';
-import { store } from '@app/lib/store';
-import { Footer, Header, Modal } from './widgets';
-import { Routing } from '@app/providers/RouterProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "@app/styles/index.scss";
+import { Provider } from "react-redux";
+import { store } from "@app/lib/store";
+import { Footer, Header, Modal } from "./widgets";
+import { FloatingWhatsApp } from "./features";
+import { Routing } from "@app/providers/RouterProvider";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Routing />
         <Footer />
         <Modal />
+        <FloatingWhatsApp />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
