aaa72ba6f29ae1ddc17c050047cbd022a3d1deb3
Saiid <128289515+fsgfdgsagdyfdu@users.noreply.github.com>
2024-02-04 21:34:50 +0600

    BenefitsGH

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import { BenefitsGH } from '@/features/BenefitsGH';
 import { FormCallBack, Property } from '@/widgets'
 
 export const Home = () => {
   return (
     <div>
       <Property />
+      <BenefitsGH />
       <FormCallBack />
     </div>
   );
---
+++ src/shared/ui/Icons/BenefitsImg2.tsx
@@ -0,0 +1,7 @@
+export const BenefitsImg2 = () => {
+    return (
+        <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M30 55C30 55 50 45 50 30V12.5L30 5L10 12.5V30C10 45 30 55 30 55Z" stroke="#C6A15B" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+    )
+}
