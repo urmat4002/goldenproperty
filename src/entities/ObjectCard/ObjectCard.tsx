3329529b628fbaa891e563404f9ab34955a9db7f
Imranio07 <kurutkg09@gmail.com>
2024-02-05 21:50:15 +0600

    Gak-128

--- src/entities/ObjectCard/ObjectCard.tsx
+++ src/entities/ObjectCard/ObjectCard.tsx
@@ -1,3 +1,26 @@
+import React from 'react';
+import styles from './ObjectCard.module.scss';
+import {createLucideIcon} from "lucide-react";
+import {MapPin} from "lucide-react";
+import {WhatsApp} from "@shared/ui/Icons";
+
 export const ObjectCard = () => {
-  return <div>ObjectCard</div>;
+  return (
+    <div className={styles.objectCard}>
+      <div className={styles.imageContainer}>
+        <img src="path-to-your-image.jpg" alt="Serena Living Tower" />
+      </div>
+      <div className={styles.content}>
+         <div className={styles.content_1}>
+            <h1>Serena Living Tower</h1>
+            <p><MapPin />Dubai, Serenia Living</p>
+            <p className={styles.price}>AED 159,000,000</p>
+
+        </div>
+          <div className={styles.content_2}>
+              <button className={styles.button_1}>Whatsapp &nbsp;<WhatsApp/></button>
+            </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,12 @@
 import { FormCallBack, Property } from '@/widgets'
+import {ObjectCard} from "@entities/ObjectCard";
+import {HeroHome} from "@widgets/Hero/HeroHome";
 
 export const Home = () => {
   return (
     <div>
+        <HeroHome/>
+        <ObjectCard/>
       <Property />
       <FormCallBack />
     </div>
