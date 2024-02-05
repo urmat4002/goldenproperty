44148d07a4ddbb60ca720087225f7469f95d6d85
Maxim <334890@gmail.com>
2024-02-05 12:16:17 +0600

    fix Select import @ Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
-import { BenefitsGH } from '@/features/BenefitsGH';
-import { FormCallBack, Property } from '@/widgets'
+import { Select } from "@/features";
+import { BenefitsGH } from "@/features/BenefitsGH";
+import { FormCallBack, Property } from "@/widgets";
 
 export const Home = () => {
   return (
     <div>
-      <Select/>
+      <Select />
       <Property />
       <BenefitsGH />
       <FormCallBack />
