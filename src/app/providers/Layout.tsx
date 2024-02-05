c5537ea19b5be13bb9b61ea444e628280d2d8a1b
dayan <dayanulanov.wer@gmail.com>
2024-02-05 14:49:47 +0600

    build: add layout style and change color

--- src/app/providers/Layout.tsx
+++ src/app/providers/Layout.tsx
@@ -2,10 +2,12 @@ import {FC} from 'react'
 import {Outlet} from 'react-router-dom'
 import {Footer, Header, Modal} from '../../widgets'
 import { FloatingWhatsApp } from '@/features'
+import styles from './style/Layout.module.scss'
+
 
 export const Layout: FC = () => {
   return (
-    <main>
+    <main className={styles.main}>
       <Header />
       <Outlet />
       <Footer />
