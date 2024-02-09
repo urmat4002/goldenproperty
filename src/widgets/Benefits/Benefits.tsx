4599cbaa5fa1ded9e0bfeec058f4339171fecaea
Saiid <128289515+fsgfdgsagdyfdu@users.noreply.github.com>
2024-02-09 22:23:46 +0600

    style: update-benefits

--- src/widgets/Benefits/Benefits.tsx
+++ src/widgets/Benefits/Benefits.tsx
@@ -16,26 +16,30 @@ export const Benefits = () => {
           <div className={style.benefitsItem}>
             <BenefitsImg1 />
             <Typography variant="h3" color="white" weight="medium">
-              A wide selection
+              <p className={style.benefitsTitle}>Exclusive offers</p>
             </Typography>
+            <p className={style.benefitsBody}>Our company offers a wide selection of real estate from exclusive apartments to country houses.</p>
           </div>
           <div className={style.benefitsItem}>
             <BenefitsImg2 />
             <Typography variant="h3" color="white" weight="medium">
-              Confidentiality
+              <p className={style.benefitsTitle}>Confidentiality</p>
             </Typography>
+            <p className={style.benefitsBody}>We guarantee the confidentiality of your data and cooperate only with trusted agents and sellers.</p>
           </div>
           <div className={style.benefitsItem}>
             <BenefitsImg3 />
             <Typography variant="h3" color="white" weight="medium">
-              Exclusive offers
+              <p className={style.benefitsTitle}>Exclusive offers</p>
             </Typography>
+            <p className={style.benefitsBody}>Exclusive offers, virtual tours, consultations with experts - everything to make the process of choosing a property convenient and exciting.</p>
           </div>
           <div className={style.benefitsItem}>
             <BenefitsImg4 />
             <Typography variant="h3" color="white" weight="medium">
-              Feedback
+              <p className={style.benefitsTitle}>Feedback</p>
             </Typography>
+            <p className={style.benefitsBody}>If you need help managing your property, we provide rental management services, ensuring your property receives maximum value and care.</p>
           </div>
         </div>
       </div>
