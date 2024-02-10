a87eaf2da9e500c5e024f981c092da32b9b8cf9e
Saiid <128289515+fsgfdgsagdyfdu@users.noreply.github.com>
2024-02-10 22:28:47 +0600

    bild: DubaiEstates

--- src/widgets/Benefits/Benefits.tsx
+++ src/widgets/Benefits/Benefits.tsx
@@ -1,6 +1,5 @@
 import style from "./Benefits.module.scss";
 import { Section } from "@/features";
-import { Typography } from "@/shared/ui";
 import {
   BenefitsImg1,
   BenefitsImg2,
@@ -8,40 +7,40 @@ import {
   BenefitsImg4,
 } from "@/shared/ui/Icons/BenefitsImg";
 
+const objectBenefits = [
+  {
+    img: <BenefitsImg1 />,
+    title: 'Exclusive offers', 
+    body: 'Our company offers a wide selection of real estate from exclusive apartments to country houses'
+  },
+  {
+    img: <BenefitsImg2 />,
+    title: 'Confidentiality',
+    body: 'We guarantee the confidentiality of your data and cooperate only with trusted agents and sellers.'
+  },
+  {
+    img: <BenefitsImg3 />,
+    title: 'A wide selection',
+    body: 'Exclusive offers, virtual tours, consultations with experts - everything to make the process of choosing a property convenient and exciting.',
+  },
+  {
+    img: <BenefitsImg4/>,
+    title: 'Feedback',
+    body: 'If you need help managing your property, we provide rental management services, ensuring your property receives maximum value and care.'
+  }
+]
+
 export const Benefits = () => {
   return (
-    <Section title="Benefits of Golden House">
+    <Section title="Benefits of Golden House" container>
       <div className={style.benefits}>
-        <div className={style.benefitsContainer}>
+        {objectBenefits.map(item =>   
           <div className={style.benefitsItem}>
-            <BenefitsImg1 />
-            <Typography variant="h3" color="white" weight="medium">
-              <p className={style.benefitsTitle}>Exclusive offers</p>
-            </Typography>
-            <p className={style.benefitsBody}>Our company offers a wide selection of real estate from exclusive apartments to country houses.</p>
+            {item.img}
+            <p className={style.benefitsTitle}>{item.title}</p>
+            <p className={style.benefitsBody}>{item.body}</p>
           </div>
-          <div className={style.benefitsItem}>
-            <BenefitsImg2 />
-            <Typography variant="h3" color="white" weight="medium">
-              <p className={style.benefitsTitle}>Confidentiality</p>
-            </Typography>
-            <p className={style.benefitsBody}>We guarantee the confidentiality of your data and cooperate only with trusted agents and sellers.</p>
-          </div>
-          <div className={style.benefitsItem}>
-            <BenefitsImg3 />
-            <Typography variant="h3" color="white" weight="medium">
-              <p className={style.benefitsTitle}>Exclusive offers</p>
-            </Typography>
-            <p className={style.benefitsBody}>Exclusive offers, virtual tours, consultations with experts - everything to make the process of choosing a property convenient and exciting.</p>
-          </div>
-          <div className={style.benefitsItem}>
-            <BenefitsImg4 />
-            <Typography variant="h3" color="white" weight="medium">
-              <p className={style.benefitsTitle}>Feedback</p>
-            </Typography>
-            <p className={style.benefitsBody}>If you need help managing your property, we provide rental management services, ensuring your property receives maximum value and care.</p>
-          </div>
-        </div>
+        )}
       </div>
     </Section>
   );
