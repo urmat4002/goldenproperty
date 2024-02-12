3866f5c2618179ec83918ed6e544df14639ae188
dayan <dayanulanov.wer@gmail.com>
2024-02-12 12:02:33 +0600

    fix: remove slider

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,11 @@
 import { Benefits } from "@/widgets/Benefits";
 import { FormCallBack, HeroHome, Property } from "@/widgets";
-import { SliderCity } from "@/features/Slider/SliderCity";
 
 export const Home = () => {
   return (
     <>
       <HeroHome />
       <Property />
-      <SliderCity />
       <Benefits />
       <FormCallBack />
     </>
