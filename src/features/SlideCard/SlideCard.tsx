833b67f7061a6f7ffbda21cc9e6f37ff106ae4ba
Saiid <128289515+fsgfdgsagdyfdu@users.noreply.github.com>
2024-02-24 21:46:29 +0600

    build: update slide-card

--- src/features/SlideCard/SlideCard.tsx
+++ src/features/SlideCard/SlideCard.tsx
@@ -1,138 +1,20 @@
 import { Section } from '..'
-import { ObjectCard } from "@/entities";
+import { ObjectCard, ObjectCardProps } from "@/entities";
 import style from './SlideCard.module.scss'
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useState } from "react";
+
 
 export const SlideCard = () => {
-  const Cards = [
-    {
-      id: 0,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-    {
-      id: 1,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-    {
-      id: 2,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-    {
-      id: 3,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-    {
-      id: 4,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-    {
-      id: 5,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-     
-    },
-    {
-      id: 6,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-    {
-      id: 7,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-                "name": "Artesia A",
-                "location": "Artesia"
-            },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-    {
-      id: 8,
-      title: 'Dubai',
-      "price_usd": 252450.0,
-      "project": {
-        "name": "Artesia A",
-        "location": "Artesia"
-      },
-      images: [
-        "http://209.38.228.54/back_media/estate/Antalya/70/VuWfMGDKnt.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/syNydGhnPy.webp",
-        "http://209.38.228.54/back_media/estate/Antalya/70/GDwXBWgVHJ.webp",
-      ]
-    },
-  ]
+  const [estates, setEstates] = useState<ObjectCardProps[]>([]);
+  useEffect(() => {
+    fetch("/api/v1/estate")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json.estates);
+        setEstates(json.estates);
+      });
+  }, [])
   return (
     <Section container>
       <Swiper
@@ -140,18 +22,18 @@ export const SlideCard = () => {
         slidesPerView={2.5}
       >
       <div className={style.Sliders}>
-          {Cards.map((item) => (
+          {estates.map((item) => (
             <SwiperSlide className={style.SlideCard}>
               <ObjectCard
                 key={item.id}
                 id={item.id}
                 images={Array.isArray(item.images) ? item.images : [item.images]}
                 price_usd={item.price_usd}
-                city={item.title}
+                city={item.city}
                 project={item.project}
               />
             </SwiperSlide>
-          ))}
+          ))} 
       </div>
       </Swiper>
     </Section>
