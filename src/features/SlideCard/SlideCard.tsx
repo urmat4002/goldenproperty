974ab496994ab800e1c958337ecbc8fa3ce6c89d
urmat4002 <urmat4002@gmail.com>
2024-02-25 13:43:20 +0600

    fix: add key and similar objects

--- src/features/SlideCard/SlideCard.tsx
+++ src/features/SlideCard/SlideCard.tsx
@@ -1,41 +1,39 @@
-import { Section } from '..'
+import { Section } from "..";
 import { ObjectCard, ObjectCardProps } from "@/entities";
-import style from './SlideCard.module.scss'
+import style from "./SlideCard.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useEffect, useState } from "react";
-
+import { useParams } from "react-router-dom";
 
 export const SlideCard = () => {
   const [estates, setEstates] = useState<ObjectCardProps[]>([]);
+  const { id } = useParams();
   useEffect(() => {
-    fetch("/api/v1/estate")
+    fetch(`/api/v1/estate/${id}/similar`)
       .then((response) => response.json())
       .then((json) => {
-        console.log(json.estates);
         setEstates(json.estates);
       });
-  }, [])
+  }, [id]);
   return (
     <Section container>
-      <Swiper
-        spaceBetween={20}
-        slidesPerView={2.5}
-      >
-      <div className={style.Sliders}>
+      <Swiper spaceBetween={20} slidesPerView={2.5}>
+        <div className={style.Sliders}>
           {estates.map((item) => (
-            <SwiperSlide className={style.SlideCard}>
+            <SwiperSlide key={item.id} className={style.SlideCard}>
               <ObjectCard
-                key={item.id}
                 id={item.id}
-                images={Array.isArray(item.images) ? item.images : [item.images]}
+                images={
+                  Array.isArray(item.images) ? item.images : [item.images]
+                }
                 price_usd={item.price_usd}
                 city={item.city}
                 project={item.project}
               />
             </SwiperSlide>
-          ))} 
-      </div>
+          ))}
+        </div>
       </Swiper>
     </Section>
-  )
-}
+  );
+};
