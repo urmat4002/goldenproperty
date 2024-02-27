ca9c1a98d0a39ae4fcfb6444f429e8f9d39cb4b9
Maxim <334890@gmail.com>
2024-02-27 20:40:30 +0600

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://209.38.228.54",
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
