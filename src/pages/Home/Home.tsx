e98493b56ca9b26961404f379e2d10a7088b0610
akktii <aktanmoldokeev123@gmail.com>
2024-03-01 16:57:47 +0600

    fix: fixed sliderCity

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Benefits } from "@/widgets/Benefits";
 import { FormCallBack, HeroHome, Property } from "@/widgets";
+import { SliderCity } from "@/features/Slider/SliderCity";
 
 export const Home = () => {
   return (
     <>
       <HeroHome />
       <Property />
+      <SliderCity />
       <Benefits />
       <FormCallBack />
     </>
