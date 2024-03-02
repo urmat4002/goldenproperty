f828ca0d527c12c6e80bf95587ede5f8db6f563b
Maxim <334890@gmail.com>
2024-03-02 22:50:18 +0600

    add comment

--- src/app/providers/Layout.tsx
+++ src/app/providers/Layout.tsx
@@ -26,6 +26,7 @@ export const Layout: FC = () => {
         </main>
         <Footer />
         <FloatingButtons />
+        {/* FIX_ME компонент Modal теперь может не принимать пропы, а брать isModalOpen и setIsModalOpen из контекста, раз уж у нас теперь есть контекст*/}
         <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
         <ScrollToTop />
       </ModalContext.Provider>
