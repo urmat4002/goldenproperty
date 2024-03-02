41b3a0a73509bdd31c6b2ac105bdeb6795aa6c3b
Imranio07 <kurutkg09@gmail.com>
2024-03-02 21:39:01 +0600

    created

--- src/shared/ui/Icons/Calendar.tsx
+++ src/shared/ui/Icons/Calendar.tsx
@@ -1,4 +1,4 @@
-export const calendar = () => {
+export const Calendar = () => {
     return (
         <svg width="37" height="36" viewBox="0 0 37 36" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd"
--- src/shared/ui/Icons/Location.tsx
+++ src/shared/ui/Icons/Location.tsx
@@ -1,4 +1,4 @@
-export const calendar = () => {
+export const Location = () => {
     return (
         <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
