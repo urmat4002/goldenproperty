15f8f3b7d662821e949b5607e8e44f2c69b67c30
akktii <aktanmoldokeev123@gmail.com>
2024-03-02 20:31:32 +0600

    build: sidebar for mobile

--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -1,19 +1,41 @@
-import { useAppDispatch } from "@/shared/hooks/hooks";
+import { useAppDispatch, useAppSelector } from "@/shared/hooks/hooks";
 import styles from "./Navbar.module.scss";
 import { NavbarData } from "./data/Navbar.data";
 import { Typography } from "@/shared/ui";
 import { NavLink } from "react-router-dom";
 import { setClose, setOpen } from "@/shared/slices/MenuCityHover/MenuCityHover";
 import { setOpenModal } from "@/shared/slices/Modal/ModalSlice";
+import { MenuLeft } from "../MenuDropdown/MenuLeft";
+import data from "./data/db.json";
+import { useState } from "react";
+import { ICity } from "./types/Navbar.types";
+import { ChevronDown } from "lucide-react";
 
 export const Navbar = () => {
   const dispatch = useAppDispatch();
+  const isOpen = useAppSelector((state) => state.menuSlice.isOpen);
+  const [cityId, setCityId] = useState<number>(0);
+  const [dataCity] = useState<ICity[]>(data);
+  const [isCityOpen, setIsCityOpen] = useState(false);
+
+  const handleCityClick = (id: number) => {
+    setCityId(id);
+  };
+
+  const toggleCity = (value: boolean) => {
+    setIsCityOpen(value);
+    dispatch(setOpen());
+  };
+
+  const toggleOpen = () => {
+    isOpen ? dispatch(setClose()) : dispatch(setOpen());
+  };
+
   return (
     <div className={styles.navbar}>
       <button
-        onMouseEnter={() => dispatch(setOpen())}
-        onMouseLeave={() => dispatch(setClose())}
-        className={styles.navbarMenuItem}
+        onClick={() => toggleOpen()}
+        className={`${styles.navbarMenuItem} ${styles.city1}`}
       >
         <Typography
           variant="body"
@@ -24,6 +46,25 @@ export const Navbar = () => {
           City
         </Typography>
       </button>
+
+      <button
+        onClick={() => toggleCity(!isCityOpen)}
+        className={`${styles.navbarMenuItem} ${styles.city2} ${isCityOpen ? styles.active : ""}`}
+      >
+        <Typography
+          variant="body"
+          weight="medium"
+          color="white"
+          className={styles.navbarMenuSelect}
+        >
+          City
+          <ChevronDown />
+        </Typography>
+      </button>
+
+      {isCityOpen ? (
+        <MenuLeft onClick={handleCityClick} data={dataCity} id={cityId} />
+      ) : null}
       <ul className={styles.navbarMenu}>
         {NavbarData.map((link) => {
           return (
--- src/features/Navbar/types/Navbar.types.ts
+++ src/features/Navbar/types/Navbar.types.ts
@@ -2,3 +2,10 @@ export interface NavbarProps {
   label: string;
   path: string;
 }
+
+export interface ICity {
+  id: number;
+  city__name: string;
+  city__description: string;
+  city__img: string;
+}
