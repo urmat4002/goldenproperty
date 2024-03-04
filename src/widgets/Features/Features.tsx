282a55cd9dccbca71a5138b379c526dd38aa174f
Maxim <334890@gmail.com>
2024-03-04 22:15:56 +0600

    fix features

--- src/widgets/Features/Features.tsx
+++ src/widgets/Features/Features.tsx
@@ -1,23 +1,28 @@
-import { Feather } from "lucide-react";
-import style from "./Features.module.scss";
-import { Section } from "@/features";
 import { useParams } from "react-router-dom";
+import { Feather } from "lucide-react";
+import { Section } from "@/features";
 import { useGetEstateById, useGetStaticData } from "@/shared/api/hooks";
 import { Typography } from "@/shared/ui";
+import { capitalize } from "@/shared/helper/utils";
+import style from "./Features.module.scss";
 
 export const Features = () => {
-  const { id } = useParams()
-  const { data } = useGetEstateById(id)
-  const { data : Static } = useGetStaticData()
-  
+  const { id } = useParams();
+  const { data } = useGetEstateById(id);
+  const { data: staticData } = useGetStaticData();
+
   return (
-    <Section title={Static?.static_data.body.features_and_amenities}>
+    <Section
+      title={capitalize(staticData?.static_data.body.features_and_amenities)}
+    >
       <div className={style.features}>
         <div className={style.featuresContainer}>
           {data?.estate.project.facilities.map((features) => (
             <div className={style.featuresItem} key={features.type}>
               {features.icon ? <img src={features.icon} alt="" /> : <Feather />}
-              <Typography variant='body' capitalize>{features.type}</Typography>
+              <Typography variant="body" capitalize>
+                {features.type}
+              </Typography>
             </div>
           ))}
         </div>
