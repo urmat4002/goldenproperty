7a3be9feb8ef95854b63644d8339c15032176a9f
Saiid <128289515+fsgfdgsagdyfdu@users.noreply.github.com>
2024-03-05 21:31:14 +0600

    add-data-aboutUs

--- src/widgets/Hero/HeroAboutUs/AboutUsSubsection/AboutUsSubsection.tsx
+++ src/widgets/Hero/HeroAboutUs/AboutUsSubsection/AboutUsSubsection.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 import { Typography } from "@/shared/ui";
 
 interface Props {
-  imageUrl: string;
+  imageUrl: string | undefined;
   title: string;
-  content: string;
+  content: string | undefined;
   styles: CSSModuleClasses;
 }
 
