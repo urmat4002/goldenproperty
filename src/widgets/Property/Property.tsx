1b34a21714c9a03c8456d77ae772c52a99da20ea
Maxim <334890@gmail.com>
2024-03-11 22:33:30 +0600

    make Property section responsive

--- src/widgets/Property/Property.tsx
+++ src/widgets/Property/Property.tsx
@@ -2,29 +2,19 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import { Typography } from "@/shared/ui";
 import { Section } from "@/features";
-//import { useInnerWidthExceedsDefault } from "@/shared/helper/ScreenWidthTracker";
 import { useGetCities, useGetStaticData } from "@/shared/api/hooks";
-import styles from "./Property.module.scss";
 import { GButton } from "@/shared/ui/Button/GButton";
+import styles from "./Property.module.scss";
 
 export const Property: FC = () => {
-  //const state = useInnerWidthExceedsDefault({ defaultThreshold: 992 });
-  //const [value, setValue] = useState<number>(0);
   const { data } = useGetCities();
-  const { data: staticData } = useGetStaticData();
-
-  // console.log(staticData?.static_data.body.see_real_estates);
+  const { staticData } = useGetStaticData();
 
   const firstCityData = data?.cities?.[0];
 
-  /* useEffect(() => {
-    setValue(state ? 520 : 320);
-    //FIX_ME: wtf effect watches value and sets value
-  }, [state, value]); */
-
   return (
     <Section
-      title={staticData?.static_data.body.property || "Property"}
+      title={staticData?.body.property || "Property"}
       customClassName={styles.property}
       container
     >
@@ -46,10 +36,12 @@ export const Property: FC = () => {
             <div className={styles.descriptionShade}></div>
           </div>
 
-          <Link to={`/estates/?city=${firstCityData?.id || "1"}`}>
-            <GButton variant="navigate">
-              {staticData?.static_data.body.see_real_estates ||
-                "See real estates"}
+          <Link
+            className={styles.button}
+            to={`/estates/?city=${firstCityData?.id || "1"}`}
+          >
+            <GButton className={styles.button} variant="navigate">
+              {staticData?.body.see_real_estates || "See real estates"}
             </GButton>
           </Link>
         </div>
