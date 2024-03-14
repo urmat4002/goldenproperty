491fd76a80529dec4f60360e1e5ec3710db82cc6
Maxim <334890@gmail.com>
2024-03-14 23:10:53 +0600

    remove redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routing } from "./app/providers/RouterProvider";
-//FIX_ME remove redux
-import { store } from "./app/lib/store";
 import "@app/styles/index.scss";
 
 const client = new QueryClient({
@@ -18,10 +15,8 @@ const client = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <QueryClientProvider client={client}>
-        <Routing />
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={client}>
+      <Routing />
+    </QueryClientProvider>
   </BrowserRouter>
 );
