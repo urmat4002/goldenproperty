e537c181400398ef20d8fa2a3432562447e97614
Maxim <334890@gmail.com>
2024-03-17 17:48:31 +0600

    remove old Button

--- src/shared/ui/Button/config/Button.config.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonConfig } from "../types/Button.types";
-
-export const buttonConfigs: ButtonConfig = {
-  primary: { classes: "button-primary-btn", variant: "contained" },
-  secondary: { classes: "button-secondary-btn", variant: "contained" },
-  link: { classes: "button-link-btn" },
-  icon: { classes: "button-icon-btn" },
-};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,3 @@
-export * from "./Button";
 export * from "./Typography";
 export * from "./Input";
 export * from "./Select";
